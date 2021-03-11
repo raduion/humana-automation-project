a5d31048f5cb0021ffe3a975ef4133f1a2c97f64
raduion <62696492+raduion@users.noreply.github.com>
2021-03-11 16:12:28 +0200

    couple of scripts

--- tests/automation_template/test_template_automation.py
+++ tests/automation_template/test_template_automation.py
@@ -1,5 +1,7 @@
 # this is a test template
 from pages.base_page import BasePage
+from pages.base_element import BaseElement
+from pages.css_selectors import CSS_SELECTORS
 import os
 from pages.mongo_db import Connect, DATABASE_CONNECTION_STRINGS
 from pymongo import MongoClient
@@ -16,24 +18,29 @@ from pages.sms_handler import SMSHandlers
 def test_template(browser):
     # 1. Go to the starting link and arrive at the landing page.
 
-    base = BasePage(browser)
-    sms = SMSHandlers()
-    email = EmailOperations()
-    db = Connect()
+    base_page = BasePage(browser)
+    base_element = BaseElement(browser)
 
-    base.load_url(subdomain='twilio-incoming-sms.', second_level_domain='herokuapp.',
-                  subdirectory='/message_history', path='',
-                  wait_element='div[id="json"]')
-    # string = sms.get_message_text(index=-2)
-    # url = sms.get_url_from_message(string)
-    # base.load_url(scheme=url,
-    #               subdomain='',
-    #               top_level_domain='',
-    #               second_level_domain='',
-    #               subdirectory='',
-    #               path='',
-    #               wait_element='div[id="app"]')
-    # long_url = sms.decode_url(url=url)
-    # sms.get_token_from_url(long_url=long_url)
+    # base_page.load_url(subdomain='tinyurl.com/ya8pvsb3',
+    #                    top_level_domain='', second_level_domain='', subdirectory='',
+    #                    path='', wait_element='div[id="app"]')
+    # base_element.button_click(button_selector=CSS_SELECTORS['footer_next'],
+    #                           wait_element=CSS_SELECTORS['footer_next'])
+    # base_element.send_user_input(input_data='ceva acolo',
+    #                              input_field=CSS_SELECTORS['text_input'],
+    #                              wait_element=CSS_SELECTORS['footer_next'])
+    # base_element.select_option_from_radio(radio_selector=CSS_SELECTORS['radio_option'],
+    #                                       option_number=0,
+    #                                       wait_element=CSS_SELECTORS['footer_next'])
+    # base_element.button_click(button_selector=CSS_SELECTORS['footer_next'],
+    #                           wait_element=CSS_SELECTORS['footer_next'])
+    # base_element.button_click(button_selector=CSS_SELECTORS['footer_next'],
+    #                           wait_element=CSS_SELECTORS['footer_next'])
 
-    url = email.get_email_url()
+    url = 'https://twilio-incoming-sms.herokuapp.com/message_list'
+    value = 'START'
+
+    requests.post(url, data=value)
+
+    # test
+
--- tests/automation_template/test_template_automation_navigation.py
+++ tests/automation_template/test_template_automation_navigation.py
@@ -1,7 +1,7 @@
 # this is a test template
 from pages.base_page import BasePage
 from pages.base_element import BaseElement
-from pages.css_selectors import CSS_SELECTORS_LIFEMAP, CSS_SELECTORS_LIFEMAP_CARDS
+from pages.css_selectors import CSS_SELECTORS, CSS_SELECTORS_LIFEMAP_CARDS
 from pages.mongo_db import Connect, DATABASE_CONNECTION_STRINGS
 from pymongo import MongoClient
 
@@ -16,12 +16,12 @@ def test_template(browser):
     base_page.load_url(subdomain='tinyurl.com/y2kco4t9',
                        top_level_domain='', second_level_domain='', subdirectory='',
                        path='', wait_element='div[id="app"]')
-    base_element.button_click(button_selector=CSS_SELECTORS_LIFEMAP['start_my_profile'],
-                              wait_element=CSS_SELECTORS_LIFEMAP['header_back'])
+    base_element.button_click(button_selector=CSS_SELECTORS['start_my_profile'],
+                              wait_element=CSS_SELECTORS['header_back'])
 
-    base_element.button_click(button_selector=CSS_SELECTORS_LIFEMAP['header_back'],
-                              wait_element=CSS_SELECTORS_LIFEMAP['start_my_profile'])
-    base_element.button_click(button_selector=CSS_SELECTORS_LIFEMAP['start_my_profile'],
+    base_element.button_click(button_selector=CSS_SELECTORS['header_back'],
+                              wait_element=CSS_SELECTORS['start_my_profile'])
+    base_element.button_click(button_selector=CSS_SELECTORS['start_my_profile'],
                               wait_element=CSS_SELECTORS_LIFEMAP_CARDS['activity_card_1'])
 
     base_element.button_click(button_selector=CSS_SELECTORS_LIFEMAP_CARDS['activity_card_1'],
@@ -33,8 +33,8 @@ def test_template(browser):
     base_element.button_click(button_selector=CSS_SELECTORS_LIFEMAP_CARDS['activity_card_4'],
                               wait_element=CSS_SELECTORS_LIFEMAP_CARDS['activity_card_5'])
     base_element.button_click(button_selector=CSS_SELECTORS_LIFEMAP_CARDS['activity_card_5'],
-                              wait_element=CSS_SELECTORS_LIFEMAP['footer_cards_next'])
-    base_element.button_click(button_selector=CSS_SELECTORS_LIFEMAP['footer_cards_next'],
+                              wait_element=CSS_SELECTORS['footer_cards_next'])
+    base_element.button_click(button_selector=CSS_SELECTORS['footer_cards_next'],
                               wait_element=CSS_SELECTORS_LIFEMAP_CARDS['activity_card_1'])
 
     base_element.button_click(button_selector=CSS_SELECTORS_LIFEMAP_CARDS['activity_card_1'],
@@ -42,55 +42,8 @@ def test_template(browser):
     base_element.button_click(button_selector=CSS_SELECTORS_LIFEMAP_CARDS['activity_card_2'],
                               wait_element=CSS_SELECTORS_LIFEMAP_CARDS['activity_card_3'])
     base_element.button_click(button_selector=CSS_SELECTORS_LIFEMAP_CARDS['activity_card_3'],
-                              wait_element=CSS_SELECTORS_LIFEMAP['footer_cards_next'])
-    base_element.button_click(button_selector=CSS_SELECTORS_LIFEMAP['footer_cards_next'],
-                              wait_element=CSS_SELECTORS_LIFEMAP_CARDS['activity_card_1'])
-
-    base_element.button_click(button_selector=CSS_SELECTORS_LIFEMAP_CARDS['activity_card_1'],
-                              wait_element=CSS_SELECTORS_LIFEMAP_CARDS['activity_card_2'])
-    base_element.button_click(button_selector=CSS_SELECTORS_LIFEMAP_CARDS['activity_card_2'],
-                              wait_element=CSS_SELECTORS_LIFEMAP_CARDS['activity_card_3'])
-    base_element.button_click(button_selector=CSS_SELECTORS_LIFEMAP_CARDS['activity_card_3'],
-                              wait_element=CSS_SELECTORS_LIFEMAP_CARDS['activity_card_4'])
-    base_element.button_click(button_selector=CSS_SELECTORS_LIFEMAP_CARDS['activity_card_4'],
-                              wait_element=CSS_SELECTORS_LIFEMAP_CARDS['activity_card_5'])
-    base_element.button_click(button_selector=CSS_SELECTORS_LIFEMAP_CARDS['activity_card_5'],
-                              wait_element=CSS_SELECTORS_LIFEMAP['footer_cards_next'])
-    base_element.button_click(button_selector=CSS_SELECTORS_LIFEMAP['footer_cards_next'],
-                              wait_element=CSS_SELECTORS_LIFEMAP_CARDS['activity_card_1'])
-
-    base_element.button_click(button_selector=CSS_SELECTORS_LIFEMAP_CARDS['activity_card_1'],
-                              wait_element=CSS_SELECTORS_LIFEMAP_CARDS['activity_card_2'])
-    base_element.button_click(button_selector=CSS_SELECTORS_LIFEMAP_CARDS['activity_card_2'],
-                              wait_element=CSS_SELECTORS_LIFEMAP_CARDS['activity_card_3'])
-    base_element.button_click(button_selector=CSS_SELECTORS_LIFEMAP_CARDS['activity_card_3'],
-                              wait_element=CSS_SELECTORS_LIFEMAP['footer_next'])
-    base_element.button_click(button_selector=CSS_SELECTORS_LIFEMAP['footer_cards_next'],
-                              wait_element=CSS_SELECTORS_LIFEMAP['footer_next'])
-
-    base_element.button_click(button_selector=CSS_SELECTORS_LIFEMAP['footer_next'],
-                              wait_element=CSS_SELECTORS_LIFEMAP_CARDS['activity_card_2'])
-
-    base_element.button_click(button_selector=CSS_SELECTORS_LIFEMAP_CARDS['activity_card_1'],
-                              wait_element=CSS_SELECTORS_LIFEMAP_CARDS['activity_card_2'])
-    base_element.button_click(button_selector=CSS_SELECTORS_LIFEMAP_CARDS['activity_card_2'],
-                              wait_element=CSS_SELECTORS_LIFEMAP_CARDS['activity_card_3'])
-    base_element.button_click(button_selector=CSS_SELECTORS_LIFEMAP_CARDS['activity_card_3'],
-                              wait_element=CSS_SELECTORS_LIFEMAP_CARDS['activity_card_4'])
-    base_element.button_click(button_selector=CSS_SELECTORS_LIFEMAP_CARDS['activity_card_4'],
-                              wait_element=CSS_SELECTORS_LIFEMAP_CARDS['activity_card_5'])
-    base_element.button_click(button_selector=CSS_SELECTORS_LIFEMAP_CARDS['activity_card_5'],
-                              wait_element=CSS_SELECTORS_LIFEMAP['footer_cards_next'])
-    base_element.button_click(button_selector=CSS_SELECTORS_LIFEMAP['footer_cards_next'],
-                              wait_element=CSS_SELECTORS_LIFEMAP_CARDS['activity_card_1'])
-
-    base_element.button_click(button_selector=CSS_SELECTORS_LIFEMAP_CARDS['activity_card_1'],
-                              wait_element=CSS_SELECTORS_LIFEMAP_CARDS['activity_card_2'])
-    base_element.button_click(button_selector=CSS_SELECTORS_LIFEMAP_CARDS['activity_card_2'],
-                              wait_element=CSS_SELECTORS_LIFEMAP_CARDS['activity_card_3'])
-    base_element.button_click(button_selector=CSS_SELECTORS_LIFEMAP_CARDS['activity_card_3'],
-                              wait_element=CSS_SELECTORS_LIFEMAP['footer_cards_next'])
-    base_element.button_click(button_selector=CSS_SELECTORS_LIFEMAP['footer_cards_next'],
+                              wait_element=CSS_SELECTORS['footer_cards_next'])
+    base_element.button_click(button_selector=CSS_SELECTORS['footer_cards_next'],
                               wait_element=CSS_SELECTORS_LIFEMAP_CARDS['activity_card_1'])
 
     base_element.button_click(button_selector=CSS_SELECTORS_LIFEMAP_CARDS['activity_card_1'],
@@ -102,8 +55,8 @@ def test_template(browser):
     base_element.button_click(button_selector=CSS_SELECTORS_LIFEMAP_CARDS['activity_card_4'],
                               wait_element=CSS_SELECTORS_LIFEMAP_CARDS['activity_card_5'])
     base_element.button_click(button_selector=CSS_SELECTORS_LIFEMAP_CARDS['activity_card_5'],
-                              wait_element=CSS_SELECTORS_LIFEMAP['footer_cards_next'])
-    base_element.button_click(button_selector=CSS_SELECTORS_LIFEMAP['footer_cards_next'],
+                              wait_element=CSS_SELECTORS['footer_cards_next'])
+    base_element.button_click(button_selector=CSS_SELECTORS['footer_cards_next'],
                               wait_element=CSS_SELECTORS_LIFEMAP_CARDS['activity_card_1'])
 
     base_element.button_click(button_selector=CSS_SELECTORS_LIFEMAP_CARDS['activity_card_1'],
@@ -111,11 +64,33 @@ def test_template(browser):
     base_element.button_click(button_selector=CSS_SELECTORS_LIFEMAP_CARDS['activity_card_2'],
                               wait_element=CSS_SELECTORS_LIFEMAP_CARDS['activity_card_3'])
     base_element.button_click(button_selector=CSS_SELECTORS_LIFEMAP_CARDS['activity_card_3'],
-                              wait_element=CSS_SELECTORS_LIFEMAP['footer_cards_next'])
-    base_element.button_click(button_selector=CSS_SELECTORS_LIFEMAP['footer_cards_next'],
-                              wait_element=CSS_SELECTORS_LIFEMAP['footer_next'])
+                              wait_element=CSS_SELECTORS['footer_next'])
+    base_element.button_click(button_selector=CSS_SELECTORS['footer_cards_next'],
+                              wait_element=CSS_SELECTORS['footer_next'])
 
-    base_element.button_click(button_selector=CSS_SELECTORS_LIFEMAP['footer_next'],
+    base_element.button_click(button_selector=CSS_SELECTORS['footer_next'],
+                              wait_element=CSS_SELECTORS_LIFEMAP_CARDS['activity_card_2'])
+
+    base_element.button_click(button_selector=CSS_SELECTORS_LIFEMAP_CARDS['activity_card_1'],
+                              wait_element=CSS_SELECTORS_LIFEMAP_CARDS['activity_card_2'])
+    base_element.button_click(button_selector=CSS_SELECTORS_LIFEMAP_CARDS['activity_card_2'],
+                              wait_element=CSS_SELECTORS_LIFEMAP_CARDS['activity_card_3'])
+    base_element.button_click(button_selector=CSS_SELECTORS_LIFEMAP_CARDS['activity_card_3'],
+                              wait_element=CSS_SELECTORS_LIFEMAP_CARDS['activity_card_4'])
+    base_element.button_click(button_selector=CSS_SELECTORS_LIFEMAP_CARDS['activity_card_4'],
+                              wait_element=CSS_SELECTORS_LIFEMAP_CARDS['activity_card_5'])
+    base_element.button_click(button_selector=CSS_SELECTORS_LIFEMAP_CARDS['activity_card_5'],
+                              wait_element=CSS_SELECTORS['footer_cards_next'])
+    base_element.button_click(button_selector=CSS_SELECTORS['footer_cards_next'],
+                              wait_element=CSS_SELECTORS_LIFEMAP_CARDS['activity_card_1'])
+
+    base_element.button_click(button_selector=CSS_SELECTORS_LIFEMAP_CARDS['activity_card_1'],
+                              wait_element=CSS_SELECTORS_LIFEMAP_CARDS['activity_card_2'])
+    base_element.button_click(button_selector=CSS_SELECTORS_LIFEMAP_CARDS['activity_card_2'],
+                              wait_element=CSS_SELECTORS_LIFEMAP_CARDS['activity_card_3'])
+    base_element.button_click(button_selector=CSS_SELECTORS_LIFEMAP_CARDS['activity_card_3'],
+                              wait_element=CSS_SELECTORS['footer_cards_next'])
+    base_element.button_click(button_selector=CSS_SELECTORS['footer_cards_next'],
                               wait_element=CSS_SELECTORS_LIFEMAP_CARDS['activity_card_1'])
 
     base_element.button_click(button_selector=CSS_SELECTORS_LIFEMAP_CARDS['activity_card_1'],
@@ -127,8 +102,8 @@ def test_template(browser):
     base_element.button_click(button_selector=CSS_SELECTORS_LIFEMAP_CARDS['activity_card_4'],
                               wait_element=CSS_SELECTORS_LIFEMAP_CARDS['activity_card_5'])
     base_element.button_click(button_selector=CSS_SELECTORS_LIFEMAP_CARDS['activity_card_5'],
-                              wait_element=CSS_SELECTORS_LIFEMAP['footer_cards_next'])
-    base_element.button_click(button_selector=CSS_SELECTORS_LIFEMAP['footer_cards_next'],
+                              wait_element=CSS_SELECTORS['footer_cards_next'])
+    base_element.button_click(button_selector=CSS_SELECTORS['footer_cards_next'],
                               wait_element=CSS_SELECTORS_LIFEMAP_CARDS['activity_card_1'])
 
     base_element.button_click(button_selector=CSS_SELECTORS_LIFEMAP_CARDS['activity_card_1'],
@@ -136,25 +111,50 @@ def test_template(browser):
     base_element.button_click(button_selector=CSS_SELECTORS_LIFEMAP_CARDS['activity_card_2'],
                               wait_element=CSS_SELECTORS_LIFEMAP_CARDS['activity_card_3'])
     base_element.button_click(button_selector=CSS_SELECTORS_LIFEMAP_CARDS['activity_card_3'],
-                              wait_element=CSS_SELECTORS_LIFEMAP['footer_cards_next'])
-    base_element.button_click(button_selector=CSS_SELECTORS_LIFEMAP['footer_cards_next'],
-                              wait_element=CSS_SELECTORS_LIFEMAP['footer_next'])
+                              wait_element=CSS_SELECTORS['footer_cards_next'])
+    base_element.button_click(button_selector=CSS_SELECTORS['footer_cards_next'],
+                              wait_element=CSS_SELECTORS['footer_next'])
 
-    base_element.button_click(button_selector=CSS_SELECTORS_LIFEMAP['footer_next'],
-                              wait_element=CSS_SELECTORS_LIFEMAP['footer_next'])
+    base_element.button_click(button_selector=CSS_SELECTORS['footer_next'],
+                              wait_element=CSS_SELECTORS_LIFEMAP_CARDS['activity_card_1'])
+
+    base_element.button_click(button_selector=CSS_SELECTORS_LIFEMAP_CARDS['activity_card_1'],
+                              wait_element=CSS_SELECTORS_LIFEMAP_CARDS['activity_card_2'])
+    base_element.button_click(button_selector=CSS_SELECTORS_LIFEMAP_CARDS['activity_card_2'],
+                              wait_element=CSS_SELECTORS_LIFEMAP_CARDS['activity_card_3'])
+    base_element.button_click(button_selector=CSS_SELECTORS_LIFEMAP_CARDS['activity_card_3'],
+                              wait_element=CSS_SELECTORS_LIFEMAP_CARDS['activity_card_4'])
+    base_element.button_click(button_selector=CSS_SELECTORS_LIFEMAP_CARDS['activity_card_4'],
+                              wait_element=CSS_SELECTORS_LIFEMAP_CARDS['activity_card_5'])
+    base_element.button_click(button_selector=CSS_SELECTORS_LIFEMAP_CARDS['activity_card_5'],
+                              wait_element=CSS_SELECTORS['footer_cards_next'])
+    base_element.button_click(button_selector=CSS_SELECTORS['footer_cards_next'],
+                              wait_element=CSS_SELECTORS_LIFEMAP_CARDS['activity_card_1'])
+
+    base_element.button_click(button_selector=CSS_SELECTORS_LIFEMAP_CARDS['activity_card_1'],
+                              wait_element=CSS_SELECTORS_LIFEMAP_CARDS['activity_card_2'])
+    base_element.button_click(button_selector=CSS_SELECTORS_LIFEMAP_CARDS['activity_card_2'],
+                              wait_element=CSS_SELECTORS_LIFEMAP_CARDS['activity_card_3'])
+    base_element.button_click(button_selector=CSS_SELECTORS_LIFEMAP_CARDS['activity_card_3'],
+                              wait_element=CSS_SELECTORS['footer_cards_next'])
+    base_element.button_click(button_selector=CSS_SELECTORS['footer_cards_next'],
+                              wait_element=CSS_SELECTORS['footer_next'])
+
+    base_element.button_click(button_selector=CSS_SELECTORS['footer_next'],
+                              wait_element=CSS_SELECTORS['footer_next'])
 
     base_page.scroll_to_bottom()
     base_element.send_user_input(input_data='04',
-                                 input_field=CSS_SELECTORS_LIFEMAP["month_input"],
-                                 wait_element=CSS_SELECTORS_LIFEMAP['footer_next'])
+                                 input_field=CSS_SELECTORS["month_input"],
+                                 wait_element=CSS_SELECTORS['footer_next'])
     base_element.send_user_input(input_data='11',
-                                 input_field=CSS_SELECTORS_LIFEMAP["day_input"],
-                                 wait_element=CSS_SELECTORS_LIFEMAP['footer_next'])
+                                 input_field=CSS_SELECTORS["day_input"],
+                                 wait_element=CSS_SELECTORS['footer_next'])
     base_element.send_user_input(input_data='1949',
-                                 input_field=CSS_SELECTORS_LIFEMAP["year_input"],
-                                 wait_element=CSS_SELECTORS_LIFEMAP['footer_next'])
-    base_element.button_click(button_selector=CSS_SELECTORS_LIFEMAP['footer_next'],
-                              wait_element=CSS_SELECTORS_LIFEMAP['take_a_survey'])
+                                 input_field=CSS_SELECTORS["year_input"],
+                                 wait_element=CSS_SELECTORS['footer_next'])
+    base_element.button_click(button_selector=CSS_SELECTORS['footer_next'],
+                              wait_element=CSS_SELECTORS['take_a_survey'])
 
 
 
